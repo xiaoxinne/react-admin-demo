a8c59e8e649d8314658a9eb5bffa441ce0b93027
feng <1310527333@qq.com>
2019-11-29 16:50:33 +0800

    init

---
+++ src/component/login/login.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import './login.scss'
+
+const Login: React.FC = () => {
+    return (
+        <div className="login-content">
+            <div className="login-form">
+                <form>
+                    <TextField 
+                        id="standard-search"
+                        label="Search field"
+                        type="search"
+                        margin="normal"/>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
